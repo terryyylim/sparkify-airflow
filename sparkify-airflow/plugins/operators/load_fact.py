ce1ddacac0d62fcba7e59e0b02d6fdcad144eb70
Terence Lim <terencelim@Terences-MacBook-Pro.local>
2019-08-22 16:02:57 +0800

    update load fact table operator

--- sparkify-airflow/plugins/operators/load_fact.py
+++ sparkify-airflow/plugins/operators/load_fact.py
@@ -2,21 +2,26 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                redshift_conn_id="",
+                sql_query="",
+                *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_query = sql_query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        """
+        Fact tables are usually so massive that they should only allow
+        append type functionality.
+        """
+        self.log.info('Executing LoadFactOperator!')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        redshift.run(self.sql_query)
